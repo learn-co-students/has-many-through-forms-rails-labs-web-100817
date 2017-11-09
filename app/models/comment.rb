61c725903df52b32c605383cb6371f44db37d1d2
MatteBru <mbruni616@gmail.com>
2017-11-09 10:22:15 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def comment_user=(ayy)
+    (self.user = User.find_or_create_by(username: ayy)) unless self.user_id
+  end
+
 end
